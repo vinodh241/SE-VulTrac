2815588cae518fee2f025e83bd97cd91be9d4258
vinodh241 <a.vinodh9072@gmail.com>
2025-08-20 12:10:43 +0530

    SE-Vultrac

--- mysql/db/appuser.sql
+++ mysql/db/appuser.sql
@@ -1,4 +1,3 @@
-CREATE USER IF NOT EXISTS 'testuser'@'%' IDENTIFIED WITH mysql_native_password BY 'VulT@123';
+CREATE USER IF NOT EXISTS 'testuser'@'%' IDENTIFIED WITH mysql_native_password BY 'Test@123';
 GRANT ALL ON cities.* TO 'testuser'@'%';
 FLUSH PRIVILEGES;
-
