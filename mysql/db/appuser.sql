39f9e702460d73f6847e59f1996d05d5919acf9a
vinodh241 <a.vinodh9072@gmail.com>
2025-08-20 12:48:33 +0530

    SE-Vultrac

--- mysql/db/appuser.sql
+++ mysql/db/appuser.sql
@@ -1,3 +1,15 @@
+-- CREATE USER 'test'@'%' IDENTIFIED BY 'Test@123';
+-- GRANT ALL PRIVILEGES ON vultrac.* TO 'test'@'%';
+-- FLUSH PRIVILEGES;
+
+-- Drop the old user
+DROP USER IF EXISTS 'test'@'%';
+
+-- Recreate with default plugin (caching_sha2_password in 8.4)
 CREATE USER 'test'@'%' IDENTIFIED BY 'Test@123';
-GRANT ALL PRIVILEGES ON vultrac.* TO 'test'@'%';
+
+-- Grant permissions
+GRANT ALL PRIVILEGES ON *.* TO 'test'@'%' WITH GRANT OPTION;
+
 FLUSH PRIVILEGES;
+
