1c556123e05dd0b76ab2f161db6485c543859a1c
vinodh241 <a.vinodh9072@gmail.com>
2025-09-01 17:03:27 +0530

    SE-VulTrac

--- mysql/db/appuser.sql
+++ mysql/db/appuser.sql
@@ -1,14 +1,3 @@
-DROP USER IF EXISTS 'test'@'localhost';
-DROP USER IF EXISTS 'test'@'%';
-
-CREATE USER 'test'@'%' IDENTIFIED WITH caching_sha2_password BY 'Test@123';
-GRANT ALL PRIVILEGES ON vultrac.* TO 'test'@'%';
+CREATE USER 'test'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'Test@123';
+GRANT ALL PRIVILEGES ON *.* TO 'test'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
-
-ALTER USER 'test'@'%' IDENTIFIED WITH mysql_native_password BY 'Test@123';
-GRANT ALL PRIVILEGES ON vultrac.* TO 'test'@'%';   
-
-
-
-
-
