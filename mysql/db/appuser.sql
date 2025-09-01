8c927f52ced1f78d0201ef363035844c29b06837
vinodh241 <a.vinodh9072@gmail.com>
2025-09-01 17:06:11 +0530

    SE-VulTrac

--- mysql/db/appuser.sql
+++ mysql/db/appuser.sql
@@ -1,3 +1,16 @@
+-- Remove old test users
+DROP USER IF EXISTS 'test'@'localhost';
+DROP USER IF EXISTS 'test'@'%';
+
+-- Create fresh user
 CREATE USER 'test'@'localhost' IDENTIFIED WITH caching_sha2_password BY 'Test@123';
+CREATE USER 'test'@'%' IDENTIFIED WITH caching_sha2_password BY 'Test@123';
+
+-- Grant privileges
 GRANT ALL PRIVILEGES ON *.* TO 'test'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'test'@'%' WITH GRANT OPTION;
+
+-- Apply changes
 FLUSH PRIVILEGES;
+
+
